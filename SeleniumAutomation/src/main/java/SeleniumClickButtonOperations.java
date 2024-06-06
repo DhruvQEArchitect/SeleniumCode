56140a331110bfcf8db5d3dd1bfde89be9241c86
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2024-06-05 20:38:29 -0400

    updating code with click of buttons

--- SeleniumAutomation/src/main/java/SeleniumClickButtonOperations.java
+++ SeleniumAutomation/src/main/java/SeleniumClickButtonOperations.java
@@ -1,4 +1,7 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumClickButtonOperations {
@@ -20,7 +23,28 @@ public class SeleniumClickButtonOperations {
         //This line is used to maximize browser
         driver.manage().window().maximize();
 
+        //We use WebElement to locate elements on a website to perform operations on it
+        WebElement clickMeButton = driver.findElement(By.xpath("//*[text()=\"Click Me\"]"));
+
+        scrollIntoElement(clickMeButton);
+        clickMeButton.click();
+
+        /**
+         * getText() method is used to get text from the webpage element which we can locate by writing xpath
+         */
+        String verifyText = driver.findElement(By.xpath("//*[text()=\"You have done a dynamic click\"]")).getText();
+        if (verifyText.equals("You have done a dynamic click")) {
+            System.out.println("Test case passed");
+        } else {
+            System.out.println("Test case failed");
+        }
+
         //To quit all instances opened by the driver during the run , we use driver.quit();
         driver.quit();
+    }
+
+    static void scrollIntoElement(WebElement element) {
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
     }
 }
