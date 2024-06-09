ebb807f9073f1365a847fd06fc3bf1501001b310
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2024-06-09 09:53:40 -0400

    handle frames in selenium

--- SeleniumAutomation/src/main/java/HandleAlertFrameWindows.java
+++ SeleniumAutomation/src/main/java/HandleAlertFrameWindows.java
@@ -21,21 +21,40 @@ public class HandleAlertFrameWindows {
         driver = new ChromeDriver();
 
         //This line is used to launch a site
-        driver.get("https://demoqa.com/alerts");
+//        driver.get("https://demoqa.com/alerts");
+//        driver.manage().window().maximize();
+//
+//        WebElement alertButton = driver.findElement(By.xpath("//button[@id='alertButton']"));
+//        SeleniumClickButtonOperations.scrollIntoElement(alertButton, driver);
+//        alertButton.click();
+//
+//        //to switch to alert and get text from alert
+//        String alertText = driver.switchTo().alert().getText();
+//        if (alertText.equals("You clicked a button"))
+//            System.out.println("passed");
+//        else System.out.println("failed");
+//
+//        //to accept an alert
+//        driver.switchTo().alert().accept();
+
+        /**
+         * Below code will handle frame
+         */
+        driver.get("https://demoqa.com/frames");
         driver.manage().window().maximize();
+        driver.switchTo().frame("frame1");
+        WebElement frame1Text = driver.findElement(By.xpath("//h1[@id='sampleHeading']"));
+        SeleniumClickButtonOperations.scrollIntoElement(frame1Text, driver);
+        System.out.println("Frame1 Text: " + frame1Text.getText());
 
-        WebElement alertButton = driver.findElement(By.xpath("//button[@id='alertButton']"));
-        SeleniumClickButtonOperations.scrollIntoElement(alertButton, driver);
-        alertButton.click();
-
-        //to switch to alert and get text from alert
-        String alertText = driver.switchTo().alert().getText();
-        if (alertText.equals("You clicked a button"))
+        /**
+         * Switch back to default frame or parent window to handle elements on main webpage outside of frame
+         */
+        driver.switchTo().defaultContent();
+        WebElement parentWinText = driver.findElement(By.xpath("//*[contains(text(),'Sample Iframe page There')]"));
+        if (parentWinText.isDisplayed()) {
             System.out.println("passed");
-        else System.out.println("failed");
-
-        //to accept an alert
-        driver.switchTo().alert().accept();
+        } else System.out.println("failed");
 
         driver.quit();
     }
