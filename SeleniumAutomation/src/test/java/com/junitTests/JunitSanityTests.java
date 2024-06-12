771fe44849c5e70de388384e915c182855370146
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2024-06-11 21:02:01 -0400

    Committing junit basic tests

--- SeleniumAutomation/src/test/java/com/junitTests/JunitSanityTests.java
+++ SeleniumAutomation/src/test/java/com/junitTests/JunitSanityTests.java
@@ -1,11 +1,21 @@
 package com.junitTests;
 
-import org.junit.Test;
+import org.junit.*;
 
 /**
- * Creating sample sanity tests
+ * Creating sample sanity tests with different annotationss
  */
 public class JunitSanityTests {
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Execute once before all tests");
+    }
+
+    @Before
+    public void beforeEachTest() {
+        System.out.println("Execute before every test");
+    }
 
     @Test
     public void sanityTest1() {
@@ -15,6 +25,16 @@ public class JunitSanityTests {
     @Test
     public void sanityTest2() {
         System.out.println("Running sanity test 2");
+    }
+
+    @After
+    public void afterEachTest() {
+        System.out.println("Execute after every test");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Execute once after all tests");
     }
 
 }
